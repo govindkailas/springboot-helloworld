e553c83beb119ca5b998d26f769bffc5bd68d561
Govind Kailathuvalappil Ramakrishnan <gkailathuvalap@gkailathuva-a01.vmware.com>
2020-10-04 17:10:37 +0530

    Added hello world springboot

---
+++ src/main/java/com/govind/helloworld/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.govind.helloworld.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World from Springboot!!";
+    }
+    @GetMapping("/message")
+    public String message(Model model) {
+        model.addAttribute("message", "This is a custom message");
+        return "message";
+    }
+}
